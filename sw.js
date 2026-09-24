8b8dc04ee36e22859e8ea10a7d7e1d9f837faa15
mamadtraore77-cmd <mamad.traore77@gmail.com>
2026-09-24 14:30:54 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'sotrama-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/logo.png',
+  '/manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Retourner la version en cache si elle existe
+        if (response) {
+          return response;
+        }
+        // Sinon faire la requête réseau
+        return fetch(event.request);
+      })
+  );
+});
